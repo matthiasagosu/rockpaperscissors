76189bc4ce1fb3819452e036f8fb57a1c9cdbdd0
Matthias Agosu <305662721+matthiasagosu@users.noreply.github.com>
2026-08-02 15:54:03 -0400

    Finish logic to play a round
    This completes a round of a game. Here we get the winner,
    display whether the human wins or loses, and update the scores.

--- index.js
+++ index.js
@@ -34,4 +34,57 @@ function getHumanChoice() {
   }
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  const thisRoundWinner = getRoundWinner(humanChoice, computerChoice);
+  displayRoundWinner(
+    thisRoundWinner[0],
+    thisRoundWinner[1],
+    thisRoundWinner[2],
+  );
+  updateScores(thisRoundWinner[0]);
+  console.log(humanScore, computerScore);
+}
+
+function getRoundWinner(humanChoice, computerChoice) {
+  if (computerChoice === "rock" && humanChoice === "paper") {
+    return ["human", "paper", "rock"];
+  } else if (computerChoice === "rock" && humanChoice === "scissors") {
+    return ["computer", "rock", "scissors"];
+  } else if (computerChoice === "paper" && humanChoice === "scissors") {
+    return ["human", "scissors", "paper"];
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    return ["computer", "paper", "rock"];
+  } else if (computerChoice === "scissors" && humanChoice === "rock") {
+    return ["human", "rock", "scissors"];
+  } else if (computerChoice === "scissors" && humanChoice === "paper") {
+    return ["computer", "scissors", "paper"];
+  } else {
+    return ["tie", computerChoice, humanChoice];
+  }
+}
+
+function displayRoundWinner(winner, winnerChoice, LoserChoice) {
+  if (winner === "tie") {
+    console.log(`This is a tie!!! You both chose ${winnerChoice}`);
+  } else if (winner === "computer") {
+    console.log(`You lose! ${winnerChoice} beats ${LoserChoice}`);
+  } else if (winner === "human") {
+    console.log(`You win! ${winnerChoice} beats ${LoserChoice}`);
+  }
+}
+
+function updateScores(winner) {
+  if (winner === "computer") {
+    computerScore += 1;
+  } else if (winner === "human") {
+    humanScore += 1;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
